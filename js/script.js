0d9ebbb2b6da943f71e2752a7ad913736481f476
jbenjaminy <young.benjamin@gmail.com>
2016-06-14 11:34:52 -0500

    Adds buttons and listener event

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-// -enter items (text box)
-// -add/remove
-// -check/uncheck
-// -add items out of total items
-
 // Strike through & color change for checked items
 // Event Listeners for the button clicks
 // Add items to the list from user input (sort ABC)
+$(document).ready(function() {
+
+	// -enter items & add to list
+	$('.input').on('click', 'button', function(event) {
+		event.preventDefault();
+		var newItem = $('#userInput').val();
+		console.log(newItem);
+		// append method on newitem with if statement
+		// if textbox is empty, adds nothing
+		// else adds to ul
+		// alphabetic sort
+
+	// check/uncheck items
+		// mousedown function on item text
+			// strikethrough text
+			// font color change to grey
+
+	// remove items
+		// $('.items') method to delete from ul
+
+	// item count
+		// checked items/total items
+	})
+});
