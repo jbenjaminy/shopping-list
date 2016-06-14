5822ef95957ffd64d7cdb39b81a13a87d2a57d8f
Sean Jun <seanjun21@gmail.com>
2016-06-14 08:22:39 -0700

    Adds commented wireframe and HTML base

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// -enter items (text box)
+// -add/remove
+// -check/uncheck
+// -add items out of total items
+
+// Strike through & color change for checked items
+// Event Listeners for the button clicks
+// Add items to the list from user input (sort ABC)
