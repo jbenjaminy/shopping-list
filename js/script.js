b2a2bb8cac7cf7977bbb7b98b1a34acb0cd70c43
jbenjaminy <young.benjamin@gmail.com>
2016-06-14 14:17:46 -0500

    Adds CSS changes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,11 @@
-// Strike through & color change for checked items
-// Event Listeners for the button clicks
-// Add items to the list from user input (sort ABC)
 $(document).ready(function() {
-
+	
 	// -enter items & add to list
 	$('.input').on('click', 'button', function(event) {
 		event.preventDefault();
 		var newItem = $('#inputBox').val();
 
 		$('.items ul').append('<li>' + newItem + " " + "<button>remove</button>" + '</li>');
-		// if textbox is empty, adds nothing
-		// else adds to ul
 		// alphabetic sort
 		// $('#inputBox')[0].reset();
 
@@ -21,6 +16,7 @@ $(document).ready(function() {
 			.css("text-decoration", "line-through"); 
 			// try putting in one css method
 	})
+
 	//remove items
 	$('.items ul').on('click', 'button', function(event) {
 		event.preventDefault();
